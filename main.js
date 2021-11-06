c965e0e3bc762250d32db2b44267e6aea503ff59
Psychoticc <justinwu314@gmail.com>
2021-11-06 17:30:09 -0400

    dummy data for chart.js

--- main.js
+++ main.js
@@ -103,7 +103,50 @@ function rankProducts(dataArray) {
   return productRankOrdered;
 }
 
+function pieChart(productRankOrdered){
 
+  let sum = 0;
+  let xVal = [];
+  let yVal = [];
+  for (var i = 0; i < productRankOrdered.length; i++) {
+    sum+=productRankOrdered[i][1];
+  }
+  if(productRankOrdered.length >= 5){
+    for(var i = 0; i < 5; i++){
+      xVal[i] = productRankedOrdered[i+1][0];
+      yVal[i] = (productRankOrdered[i+1][1]);
+    } 
+  } else {
+    for(var i = 0; i < productRankOrdered.length+1; i++){
+      xVal[i] = productRankOrdered[i+1][0];
+      yVal[i] = (productRankOrdered[i+1][1]);
+    }
+  }
+  var barColors = [
+    "#b91d47",
+    "#00aba9",
+    "#2b5797",
+    "#e8c3b9",
+    "#1e7145"
+  ];
+  
+  new Chart("myChart", {
+    type: "doughnut",
+    data: {
+      labels: xValues,
+      datasets: [{
+        backgroundColor: barColors,
+        data: yValues
+      }]
+    },
+    options: {
+      title: {
+        display: true,
+        text: "Most Prescribed Products"
+      }
+    }
+  });
+}
 
 myForm.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -114,8 +157,10 @@ myForm.addEventListener("submit", function (e) {
     const text = e.target.result;
     const data = csvToArray(text);
     console.log(data);
+    //console.log(pieChart(rankProducts(data)))
     console.log(getTotalsNRX(data))
-    console.log(rankProducts(data))
+    //console.log(rankProducts(data))
+    //console.log(pieChart)
     console.log(rankDoctors(getTotalsNRX(data),"Ohio",data))
     document.getElementById("landingPage").style.display = "none";
     document.getElementById("analysisPage").style.display = "block";

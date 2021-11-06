ec573e943697d6565776224f7bd4cb0d438bb7d7
muhammada1 <61174541+muhammada1@users.noreply.github.com>
2021-11-06 14:18:44 -0400

    ProductRank Method Added

--- main.js
+++ main.js
@@ -32,6 +32,31 @@ function getTotals(dataArray) {
   return doctors;
 }
 
+function rankProducts(dataArray) {
+  let productRank = {};
+  dataArray.forEach(item => {
+    if(productRank[item.Product] === undefined) {
+      //Creates an item in the map
+      productRank[item.Product] =  Number(item.TRx_Month_1) + Number(item.TRx_Month_2) + Number(item.TRx_Month_3) + Number(item.TRx_Month_4) + Number(item.TRx_Month_5) + Number(item.TRx_Month_6);
+    } else {
+      //Adds additional items into the map
+      productRank[item.Product] +=  Number(item.TRx_Month_1) + Number(item.TRx_Month_2) + Number(item.TRx_Month_3) + Number(item.TRx_Month_4) + Number(item.TRx_Month_5) + Number(item.TRx_Month_6);
+    }
+  })
+  delete productRank['undefined'];
+  var productRankOrdered = [];
+  for (var product in productRank) {
+    productRankOrdered.push([product, productRank[product]]);
+  } 
+
+  productRankOrdered.sort(function(a, b) {
+      return b[1] - a[1];
+  });
+  return productRankOrdered;
+}
+
+
+
 myForm.addEventListener("submit", function (e) {
   e.preventDefault();
   const input = csvFile.files[0];
@@ -42,6 +67,7 @@ myForm.addEventListener("submit", function (e) {
     const data = csvToArray(text);
     console.log(data);
     console.log(getTotals(data))
+    console.log(rankProducts(data))
     //document.write(JSON.stringify(data));
   };
 
